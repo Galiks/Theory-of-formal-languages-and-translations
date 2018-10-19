a4a17160d23097f0f0e836d60933e6171a69a531
galiks <turchenkov.pasha@list.ru>
2018-10-19 18:46:40 +0400

    Update FSM/ThirdTask.cs

--- FSM/ThirdTask.cs
+++ FSM/ThirdTask.cs
@@ -8,5 +8,16 @@ namespace FSM
 {
     class ThirdTask
     {
+        string path1 = @"";
+        string path2 = @"";
+        string path3 = @"";
+        string path4 = @"";
+        string path5 = @"";
+        string path6 = @"";
+        string path7 = @"";
+        string path8 = @"";
+        string path9 = @"";
+        string path10 = @"";
+        string path11 = @"";
     }
 }
